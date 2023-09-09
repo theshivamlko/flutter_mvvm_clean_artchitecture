2aa7b619f1f3ecd7a5b188a003a27ec8dcb94e87
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2023-09-09 14:58:16 +0530

    add flutter modules

--- lib/mvp/presenter/Page1.dart
+++ lib/mvp/presenter/Page1.dart
@@ -1,4 +1,6 @@
+import 'package:cutome_painter_flutter/MyCustomPainter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_module/Calculation.dart';
 import 'package:flutter_mvvm_clean_artchitecture/mvp/data/cryptodata.dart';
 import 'package:flutter_mvvm_clean_artchitecture/mvp/modules/crypto_presenter.dart';
 
@@ -24,22 +26,37 @@ class _Page1State extends State<Page1> implements CryptoListView {
 
   @override
   Widget build(BuildContext context) {
+    Size size=MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(),
       body: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          ListView.builder(
-            shrinkWrap: true,
-            itemCount: list.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
+          Container(
+            height: 300,
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: list.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
                     "${list[index].name} ${list[index].price} ${list[index].perChange}",
-                style: TextStyle(fontSize: 18),),
-              );
-            },
+                    style: TextStyle(fontSize: 18),
+                  ),
+                );
+              },
+            ),
+          ),
+          Container(
+            height: size.width,
+            width: size.width,
+            color: Colors.amber,
+            child: CustomPaint(
+              size: Size(size.width, 200),
+              painter: MyCustomPainter(),
+              child: Text("${Calculation().sqrt(256)}",style: TextStyle(fontSize: 30),),
+            ),
           ),
         ],
       ),
@@ -52,9 +69,9 @@ class _Page1State extends State<Page1> implements CryptoListView {
   @override
   void onLoadComplete(List<CryptoData> list) {
     print("onLoadComplete");
-   setState(() {
-     this.list=list;
-   });
+    setState(() {
+      this.list = list;
+    });
   }
 
   @override
